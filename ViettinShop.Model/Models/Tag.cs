f2cfcb8524245535bd19f323c827a5a313a72421
DESKTOP-UAQ1PI3\Quoc Tran <tranvanquocvq@gmail.com>
2018-09-03 11:23:12 +0700

    add domain entity for project

---
+++ ViettinShop.Model/Models/Tag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViettinShop.Model.Models
+{
+    [Table ("Tags")]
+   public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Type { set; get; }
+    }
+}
---
+++ ViettinShop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViettinShop.Model.Models
+{
+    [Table ("VisitorStatistics")]
+   public class VisitorStatistic
+    {
+        [Key]
+        
+        public Guid ID { set; get; }
+        public DateTime? VisitedDate { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
